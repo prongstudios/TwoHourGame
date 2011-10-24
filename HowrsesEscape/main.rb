d2a1d1dbdb8d9d01e07b3c59aff054de53d504eb
Will Luongo <me@willluongo.com>
2011-10-23 20:06:39 -0400

    prep for background

--- HowrsesEscape/main.rb
+++ HowrsesEscape/main.rb
@@ -5,6 +5,7 @@ class GameWindow < Gosu::Window
   def initialize
     super 1280, 1024, false
     self.caption = "The Howses Escape"
+    @background_tiles = Gosu::Image.load_tiles(self, "media/background.png", 100, 100, true)
   end
 end
 
