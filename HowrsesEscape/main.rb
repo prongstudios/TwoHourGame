cb8c1ed4c3f2fc59163fe7dddc303e31e7d224dc
Will Luongo <me@willluongo.com>
2011-10-23 20:04:35 -0400

    Added basic window class

--- HowrsesEscape/main.rb
+++ HowrsesEscape/main.rb
@@ -1,2 +1,12 @@
-require 'gems'
+require 'rubygems'
 require 'gosu'
+
+class GameWindow < Gosu::Window
+  def initialize
+    super 1280, 1024, false
+    self.caption = "The Howses Escape"
+  end
+end
+
+window = GameWindow.new
+window.show
